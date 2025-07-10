c35ec4b7158744238f3ea50a39cbc90533938f73
Samarthanand Kamble <samarthanand.kamble@webileapps.com>
2025-07-11 00:19:23 +0530

    Implement MongoDB connection and data retrieval in database.js

--- database.js
+++ database.js
@@ -1,2 +1,24 @@
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
 const dbURI = process.env.DB_CONNECTION_STRING;
+
+const client = new MongoClient(dbURI)
+
+async function run() {
+  try {
+      await client.connect();
+        const database = client.db('Hello_World');
+    const movies = database.collection('Users');
+    
+    //Read 
+    const collection = await movies.find({}).toArray();
+    console.log("Collection:", collection);
+        
+    } finally {
+      await client.close();
+    }
+}
+  console.log("MongoClient",MongoClient);
+  
+  run().catch(console.dir);
