b66afa213187fc6bef885b71df3466f22dcd0ea5
Samarthanand Kamble <samarthanand.kamble@webileapps.com>
2025-07-12 13:26:23 +0530

    Implement user insertion, deletion, and update operations

--- database.js
+++ database.js
@@ -9,16 +9,47 @@ async function run() {
   try {
       await client.connect();
         const database = client.db('Hello_World');
-    const movies = database.collection('Users');
+    const users = database.collection('Users');
     
+    //Insert
+    const newUser = {
+      name: "Samarth",
+      age: 22,
+      dob: "10/02/2001",
+      city:"Pune"
+    }
+
+    // const inserted = await database.collection('Users').insertOne(newUser);
+    // console.log("inserted:", inserted);
+
+    //Delete
+
+    // const deletedDoc = await users.deleteMany({ age: 22 });
+    // console.log("Deleted Document:", deletedDoc);
+
+
+    // const insertedNewUser = await users.insertOne(newUser)
+    // console.log("Inserted New User:", insertedNewUser);
+    
+    // Update
+    
+const updatedUser = await users.updateOne(
+      { name: "Samarthanand Kamble" },
+      { $set:{ age:24} }
+);
+    console.log("Updated User:", updatedUser);
+    
+
+
     //Read 
-    const collection = await movies.find({}).toArray();
-    console.log("Collection:", collection);
+    const newCollection = await users.find({ }).toArray();
+    console.log("newCollection", newCollection);
+    
+    
         
     } finally {
       await client.close();
     }
 }
-  console.log("MongoClient",MongoClient);
   
   run().catch(console.dir);
